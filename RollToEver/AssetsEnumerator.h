51c489eb5c9dd26fd4290ddf4ba9457155761dd8
fifnel <fifnel@gmail.com>
2012-02-11 01:18:12 +0900

    アセット列挙クラスの追加

---
+++ RollToEver/AssetsEnumerator.h
@@ -0,0 +1,30 @@
+//
+//  AssetsEnumerator.h
+//  RollToEver
+//
+//  Created by fifnel on 2012/02/10.
+//  Copyright (c) 2012年 fifnel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <AssetsLibrary/ALAssetsLibrary.h>
+
+@interface AssetsEnumerator : NSObject
+
+@property (retain) ALAssetsLibrary *assetsLibrary;
+@property (retain) id delegate;
+
+- (void) startEnumeration;
+
+@end
+
+
+@interface NSObject(AssetsEnumeratorDelegate)
+
+- (void)AssetsEnumerationStart:(NSInteger)count;
+- (void)AssetsEnumerationEnd;
+- (void)AssetsEnumerationFind:(ALAsset *)asset index:(NSInteger)index;
+- (void)AssetsEnumerationFailure:(NSError *)error;
+
+@end
---
+++ RollToEverTests/AssetsEnumeratorTest.m
@@ -0,0 +1,48 @@
+//
+//  AssetsEnumeratorTest.m
+//  RollToEver
+//
+//  Created by fifnel on 2012/02/11.
+//  Copyright (c) 2012年 fifnel. All rights reserved.
+//
+
+#import "AssetsEnumeratorTest.h"
+
+#import "AssetsEnumerator.h"
+
+#import <AssetsLibrary/ALAsset.h>
+#import <AssetsLibrary/ALAssetsGroup.h>
+#import <AssetsLibrary/ALAssetsFilter.h>
+#import <AssetsLibrary/ALAssetRepresentation.h>
+
+@implementation AssetsEnumeratorTest
+
+- (void)AssetsEnumerationStart:(NSInteger)count {
+    NSLog(@"AssetsEnumerationStart:%d", count);
+}
+
+- (void)AssetsEnumerationEnd {
+    NSLog(@"AssetsEnumerationEnd");
+}
+
+- (void)AssetsEnumerationFind:(ALAsset *)asset index:(NSInteger)index {
+    NSString *url = [asset valueForProperty:ALAssetPropertyURLs];
+    NSString *date = [asset valueForProperty:ALAssetPropertyDate];
+    NSLog(@"AssetsEnumerationFind:%@ %@", url, date);
+}
+
+- (void)AssetsEnumerationFailure:(NSError *)error {
+    NSLog(@"AssetsEnumerationFailure:%@", error);
+}
+
+
+- (void)testAssetsEnumerator
+{
+    AssetsEnumerator *ae = [[AssetsEnumerator alloc] init];
+    ae.delegate = self;
+    [ae startEnumeration];
+
+    sleep(10);
+}
+
+@end
