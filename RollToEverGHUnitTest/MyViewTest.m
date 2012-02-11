b2a1c95d3fcd4c7484f502c37e187e4f0c5e3590
fifnel <fifnel@gmail.com>
2012-02-12 01:33:46 +0900

    GHUnitを組み込んでみた

---
+++ RollToEverGHUnitTest/MyViewTest.m
@@ -0,0 +1,24 @@
+//
+//  MyViewTest.m
+//  MyTestable
+//
+//  Created by John Boiles on 10/26/11.
+//  Copyright (c) 2011. All rights reserved.
+//
+
+#import <GHUnitIOS/GHUnit.h> 
+#import "ViewController.h"
+
+@interface MyViewTest : GHViewTestCase { }
+@end
+
+@implementation MyViewTest
+
+- (void)testIPhoneViewController {
+    /*
+  ViewController *viewController = [[ViewController alloc] init];
+  GHVerifyView(viewController.view);
+     */
+}
+
+@end
---
+++ RollToEverGHUnitTest/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  RollToEverGHUnitTest
+//
+//  Created by fifnel on 2012/02/12.
+//  Copyright (c) 2012年 fifnel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"GHUnitIOSAppDelegate");
+    [pool release];
+    return retVal;
+}
