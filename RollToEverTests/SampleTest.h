4f6a7258b3d3eedb4d43901048e3d709fa0d022e
fifnel <fifnel@gmail.com>
2013-02-06 07:48:34 +0900

    SenTestingKitのテストコードで通らない物をすべて削除しました

---
+++ RollToEverTests/SampleTest.h
@@ -0,0 +1,13 @@
+//
+//  SampleTest.h
+//  RollToEver
+//
+//  Created by fifnel on 2013/02/06.
+//  Copyright (c) 2013年 fifnel. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SampleTest : SenTestCase
+
+@end
---
+++ RollToEverTests/SampleTest.m
@@ -0,0 +1,28 @@
+//
+//  SampleTest.m
+//  RollToEver
+//
+//  Created by fifnel on 2013/02/06.
+//  Copyright (c) 2013年 fifnel. All rights reserved.
+//
+
+#import "SampleTest.h"
+
+@implementation SampleTest
+
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+@end
