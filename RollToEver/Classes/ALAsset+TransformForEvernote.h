16f8f1de96388bbb7e9e5dfce0c357d7ab27cd1d
fifnel <fifnel@gmail.com>
2013-02-05 23:36:21 +0900

    コードの整理

--- RollToEver/Classes/ALAsset+TransformForEvernote.h
+++ RollToEver/Classes/ALAsset+TransformForEvernote.h
@@ -10,8 +10,23 @@
 
 @interface ALAsset (TransformForEvernote)
 
-// アセット内の写真をリサイズして生データを返す
-// maxPixelが0以下の場合はオリジナルサイズのまま返す
-- (NSData *)resizedImageData:(NSInteger)maxPixel;
+    // ファイル名の取得
+    - (NSString *)filename;
+
+    // ファイルの拡張子の取得
+    - (NSString *)fileExtension;
+
+    // UTType(CGImage生成時のフォーマット指定)の取得 
+    - (CFStringRef)UTType;
+
+    // リサイズ比率の取得
+    - (float)resizeRatio:(NSInteger)maxPixel;
+
+    // 回転方向の取得
+    - (float)orientation;
+
+    // アセット内の写真をEvernote向けに変換して生データを返す
+    // maxPixelが0以下の場合はオリジナルサイズのまま返す
+    - (NSData *)transformForEvernote:(NSInteger)maxPixel;
 
 @end
--- RollToEver/Classes/ALAsset+TransformForEvernote.m
+++ RollToEver/Classes/ALAsset+TransformForEvernote.m
@@ -68,47 +68,7 @@
     return orientation;
 }
 
-- (NSDictionary *)fixedMetadata
-{
-    static NSString *META_ROOT_ORIENTATION = @"Orientation";
-    static NSString *META_TIFF             = @"{TIFF}";
-    static NSString *META_TIFF_ORIENTATION = @"Orientation";
-
-    ALAssetRepresentation *rep = [self defaultRepresentation];
-    NSMutableDictionary *metaData = [[NSMutableDictionary alloc] initWithDictionary:[rep metadata]];
-
-    if ([metaData valueForKey:META_ROOT_ORIENTATION] != nil) {
-        [metaData setValue:[NSNumber numberWithInt:1] forKey:META_ROOT_ORIENTATION];
-    }
-    if ([metaData valueForKey:META_TIFF] != nil) {
-        if ([metaData valueForKey:META_TIFF_ORIENTATION] != nil) {
-            [[metaData valueForKey:META_TIFF] setValue:[NSNumber numberWithInt:1] forKey:META_TIFF_ORIENTATION];
-        }
-    }
-
-    return metaData;
-}
-
-+ (CFStringRef)stringToUTType:(NSString *)extension
-{
-    // TODO 他のフォーマットにも対応したい bmpとかjpg2000とか
-    // TODO pairのようなフォーマットを使ってもう少しスマートに書きたい
-    if ([extension caseInsensitiveCompare:@"jpg"] == NSOrderedSame) {
-        return kUTTypeJPEG;
-    } else if ([extension caseInsensitiveCompare:@"png"] == NSOrderedSame) {
-        return kUTTypePNG;
-    } else if ([extension caseInsensitiveCompare:@"gif"] == NSOrderedSame) {
-        return kUTTypeGIF;
-    } else {
-        // 未対応フォーマット
-        return NULL;
-    }
-}
-
-// TODO 関数名変える
-// 中で変換処理が行われることが想像できる名前が良い 
-// transformとか 
-- (NSData *)resizedImageData:(NSInteger)maxPixel
+- (NSData *)transformForEvernote:(NSInteger)maxPixel
 {
     ALAssetRepresentation *rep = [self defaultRepresentation];
 
@@ -131,35 +91,79 @@
     return resizedImageData;
 }
 
+//------------------------------------------------------------
+// 内部メソッド
+//------------------------------------------------------------
+
+// Evernote書き出し用に適したEXIFに変換する
+- (NSDictionary *)fixedMetadata
+{
+    static NSString *META_ROOT_ORIENTATION = @"Orientation";
+    static NSString *META_TIFF             = @"{TIFF}";
+    static NSString *META_TIFF_ORIENTATION = @"Orientation";
+
+    ALAssetRepresentation *rep = [self defaultRepresentation];
+    NSMutableDictionary *metaData = [[NSMutableDictionary alloc] initWithDictionary:[rep metadata]];
+
+    if ([metaData valueForKey:META_ROOT_ORIENTATION] != nil) {
+        [metaData setValue:[NSNumber numberWithInt:1] forKey:META_ROOT_ORIENTATION];
+    }
+    if ([metaData valueForKey:META_TIFF] != nil) {
+        if ([metaData valueForKey:META_TIFF_ORIENTATION] != nil) {
+            [[metaData valueForKey:META_TIFF] setValue:[NSNumber numberWithInt:1] forKey:META_TIFF_ORIENTATION];
+        }
+    }
+
+    return metaData;
+}
+
+// 拡張子文字列をUTTypeに変換する
++ (CFStringRef)stringToUTType:(NSString *)extension
+{
+    // TODO 他のフォーマットにも対応したい bmpとかjpg2000とか
+    // TODO pairのようなフォーマットを使ってもう少しスマートに書きたい
+    if ([extension caseInsensitiveCompare:@"jpg"] == NSOrderedSame) {
+        return kUTTypeJPEG;
+    } else if ([extension caseInsensitiveCompare:@"png"] == NSOrderedSame) {
+        return kUTTypePNG;
+    } else if ([extension caseInsensitiveCompare:@"gif"] == NSOrderedSame) {
+        return kUTTypeGIF;
+    } else {
+        // 未対応フォーマット
+        return NULL;
+    }
+}
+
+// 拡縮と回転（EXIFの向きをUpにして画像の向きを直す)
 + (UIImage *)scaleAndRotateImage:(CGImageRef)imageRef orientation:(int)orientation resizeRatio:(float)resizeRatio
 {
     CGFloat width = CGImageGetWidth(imageRef);
     CGFloat height = CGImageGetHeight(imageRef);
-    
+
     CGAffineTransform transform = CGAffineTransformIdentity;
     CGRect bounds = CGRectMake(0, 0, width, height);
     CGFloat boundHeight;
     switch (orientation) {
-            
+
         case 1: //UIImageOrientationUp: //EXIF = 1
             transform = CGAffineTransformIdentity;
             break;
-            
+
         case 2: //UIImageOrientationUpMirrored: //EXIF = 2
             transform = CGAffineTransformMakeTranslation(width, 0.0);
             transform = CGAffineTransformScale(transform, -1.0, 1.0);
             break;
-            
+
         case 3: //UIImageOrientationDown: //EXIF = 3
             transform = CGAffineTransformMakeTranslation(width, height);
             transform = CGAffineTransformRotate(transform, M_PI);
             break;
-            
+
         case 4: //UIImageOrientationDownMirrored: //EXIF = 4
             transform = CGAffineTransformMakeTranslation(0.0, height);
             transform = CGAffineTransformScale(transform, 1.0, -1.0);
             break;
-            
+
         case 5: //UIImageOrientationLeftMirrored: //EXIF = 5
             boundHeight = bounds.size.height;
             bounds.size.height = bounds.size.width;
@@ -168,7 +172,7 @@
             transform = CGAffineTransformScale(transform, -1.0, 1.0);
             transform = CGAffineTransformRotate(transform, 3.0 * M_PI / 2.0);
             break;
-            
+
         case 6: //UIImageOrientationLeft: //EXIF = 6
             boundHeight = bounds.size.height;
             bounds.size.height = bounds.size.width;
@@ -176,7 +180,7 @@
             transform = CGAffineTransformMakeTranslation(0.0, width);
             transform = CGAffineTransformRotate(transform, 3.0 * M_PI / 2.0);
             break;
-            
+
         case 7: //UIImageOrientationRightMirrored: //EXIF = 7
             boundHeight = bounds.size.height;
             bounds.size.height = bounds.size.width;
@@ -184,7 +188,7 @@
             transform = CGAffineTransformMakeScale(-1.0, 1.0);
             transform = CGAffineTransformRotate(transform, M_PI / 2.0);
             break;
-            
+
         case 8: //UIImageOrientationRight: //EXIF = 8
             boundHeight = bounds.size.height;
             bounds.size.height = bounds.size.width;
@@ -192,18 +196,18 @@
             transform = CGAffineTransformMakeTranslation(height, 0.0);
             transform = CGAffineTransformRotate(transform, M_PI / 2.0);
             break;
-            
+
         default:
             [NSException raise:NSInternalInconsistencyException format:@"Invalid image orientation"];
-            
+
     }
-    
+
     CGSize resizedSize = CGSizeMake(bounds.size.width * resizeRatio, bounds.size.height * resizeRatio);
-    
+
     UIGraphicsBeginImageContext(resizedSize);
-    
+
     CGContextRef context = UIGraphicsGetCurrentContext();
-    
+
     // Y軸方向に一度画像を反転させてから回転処理(CGうんたらとUIうんたらで座標系がY逆らしい）
     CGContextScaleCTM(context, resizeRatio, -resizeRatio);
     CGContextTranslateCTM(context, 0, -bounds.size.height);
