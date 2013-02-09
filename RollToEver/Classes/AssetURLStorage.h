f252e0a2205ccda7d27d178e9e24dd2b866b7ede
fifnel <fifnel@gmail.com>
2013-02-09 22:35:22 +0900

    TODOコメントの追加

--- RollToEver/Classes/AssetURLStorage.h
+++ RollToEver/Classes/AssetURLStorage.h
@@ -10,6 +10,8 @@
 // AssetURLModel　とかの方が良い
 // TODO isExistURLの処理が重くないか検討する
 // 毎回取りに行くのが重くないのか
+// TODO 今の状態だとインスタンスメソッドにしている意味が無い
+// クラスメソッドにするか、メンバ変数を使って効率化を模索するか、検討が必要
 
 #import <Foundation/Foundation.h>
 
